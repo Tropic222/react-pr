88c9fe8df0adde1fa431427f12b6de760c31359e
Tropic222 <deputat1120@gmail.com>
2024-06-01 15:01:49 +0300

    standard js counter

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,7 +1,19 @@
 function App() {
+  let likes = 0;
+  function increment() {
+    likes += 1;
+    console.log(likes);
+  }
+
+  function dicrement() {
+    likes -= 1;
+    console.log(likes);
+  }
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1>{likes}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={dicrement}>Dicrement</button>
     </div>
   );
 }
