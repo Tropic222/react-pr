b68fd33900155eb61f2a3d285d1204e9dd120280
Tropic222 <deputat1120@gmail.com>
2024-06-01 18:46:31 +0300

    put the list into a separate component

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
 import "./style/styles.css";
-import PostItem from "./components/PostItem";
+// import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JS1", body: "Description" },
+    { id: 2, title: "JS2", body: "Description" },
+    { id: 3, title: "JS3", body: "Description" },
+  ]);
+
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: "JS", body: "Description" }} />
+      <PostList posts={posts} />
     </div>
   );
 }
