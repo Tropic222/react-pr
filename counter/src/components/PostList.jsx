d3280fc5309a5117e94472278663b5af87595ac4
Tropic222 <deputat1120@gmail.com>
2024-06-12 14:36:32 +0300

    added animation to the post

--- counter/src/components/PostList.jsx
+++ counter/src/components/PostList.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import PostItem  from "./PostItem";
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
 function PostList({posts, title,  remove}) {
 
   if(!posts.length) {
     return (
-      <h1 style={{textAlign: 'center'}}> Посты не найдены</h1>
+      <h1 style={{textAlign: 'center'}}> 
+        Посты не найдены
+      </h1>
+      
     )
   }
 
 return(
     <div>
-<h1 style={{ textAlign: "center" }}>{title}</h1>
-      {posts.map((post, index) => (
-        <PostItem remove={remove} number={index + 1} key={post.id} post={post} />
-      ))}
-</div>
+      <h1 style={{ textAlign: "center" }}>
+        {title}
+      </h1>
+      <TransitionGroup>
+          {posts.map((post, index) => 
+          <CSSTransition
+          key={post.id}
+          // nodeRef={nodeRef}
+          timeout={500}
+          classNames="post"
+        >
+            
+          <PostItem remove={remove} number={index + 1}   post={post} />
+          </CSSTransition>
+          )}
+      </TransitionGroup>
+      
+    </div>
 )
 }
 
