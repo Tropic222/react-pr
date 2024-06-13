f61dc9ede36da54625b76b9f32f6cd7eafcb348d
Tropic222 <deputat1120@gmail.com>
2024-06-13 17:09:21 +0300

    added uploading posts from the api using axios and the useEffect hook

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./style/styles.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,6 +7,7 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import { usePosts } from "./hooks/usePosts";
+import PostServies from "./API/PostServies";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -17,11 +19,23 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLloading, setIsPostsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
+
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    const posts = await PostServies.getAll();
+    setPosts(posts);
+    setIsPostsLoading(false);
+  }
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -29,6 +43,7 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick={fetchPosts}>GET POST</button>
       <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         Создать пользователя
       </MyButton>
@@ -38,11 +53,15 @@ function App() {
       </MyModal>
       <hr style={{ margin: "15px 0" }}></hr>
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="Список постов JS"
-      />
+      {isPostsLloading ? (
+        <h1>Идёт загркзка...</h1>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Список постов JS"
+        />
+      )}
     </div>
   );
 }
