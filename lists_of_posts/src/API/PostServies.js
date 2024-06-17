e69648197277e6d5e30432bdd5552183150c727e
Tropic <deputat1120@gmail.com>
2024-06-17 23:15:33 +0300

    implemented loading comments to a post

--- lists_of_posts/src/API/PostServies.js
+++ lists_of_posts/src/API/PostServies.js
@@ -14,9 +14,18 @@ export default class PostServies {
 
     return response;
   }
+
   static async getById(id) {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/posts/" + id
+    );
+
+    return response;
+  }
+
+  static async getCommentsByPostId(id) {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${id}/comments`
     );
 
     return response;
--- lists_of_posts/src/pages/PostIdPage.jsx
+++ lists_of_posts/src/pages/PostIdPage.jsx
@@ -7,12 +7,18 @@ import Loader from "../components/UI/Loader/Loader";
 const PostIdPage = () => {
     const params = useParams();
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFatching(async(id) => {
         const response = await PostServies.getById(id)
         setPost(response.data)
     })
+    const [fetchComments, isComLoading, comerror] = useFatching(async(id) => {
+        const response = await PostServies.getCommentsByPostId(id)
+        setComments(response.data)
+    })
     useEffect(() =>{
         fetchPostById(params.id)
+        fetchComments(params.id)
     }, [])
     return (
         <div>
@@ -21,6 +27,17 @@ const PostIdPage = () => {
             ?<Loader/>
             :<div>{post.id}{post.title}</div>
 
+        }
+        <h1>Комментарии</h1>
+        {isComLoading
+            ?<Loader/>
+            : <div>{comments.map(comm => 
+                    <div style={{marginTop: 15}}>
+                        <h5>{comm.email}</h5>
+                        <div>{comm.body}</div>
+                    </div> 
+                )}
+                </div>
         }
         </div>
     );
