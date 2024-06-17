913772588e98600dd69b6ca4dcdcf5a01571e2c1
Tropic222 <deputat1120@gmail.com>
2024-06-17 17:37:29 +0300

    the AppRouter routing is ready

--- lists_of_posts/src/App.js
+++ lists_of_posts/src/App.js
@@ -1,34 +1,14 @@
 import React from "react";
 import "./style/styles.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
 
-// import PostList from "./components/PostList";
-// import PostForm from "./components/PostForm";
-// import PostFilter from "./components/PostFilter";
-// import MyModal from "./components/UI/MyModal/MyModal";
-// import MyButton from "./components/UI/button/MyButton";
-// import { usePosts } from "./hooks/usePosts";
-// import PostServies from "./API/PostServies";
-// import Loader from "./components/UI/Loader/Loader";
-// import { useFatching } from "./hooks/useFetching";
-// import { getPageCount } from "./utils/pages";
-// import Pagination from "./components/UI/pagination/Pagination";
-
+import Navbar from "./API/Navbar";
+import AppRouter from "./components/AppRouter";
 function App() {
   return (
     <BrowserRouter>
-      <div className="navbar">
-        <div className="navbar__links">
-          <a href="/about">About</a>
-          <a href="/posts">Posts</a>
-        </div>
-      </div>
-      <Routes>
-        <Route path="posts" element={<Posts />} />
-        <Route path="about" element={<About />} />
-      </Routes>
+      <Navbar />
+      <AppRouter />
     </BrowserRouter>
   );
 }
---
+++ lists_of_posts/src/components/AppRouter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "../pages/About";
+import Posts from "../pages/Posts";
+import Error from "../pages/Error";
+const AppRouter = () => {
+    return (
+        <Routes>
+        <Route path="posts" element={<Posts />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    )
+}
+
+export default AppRouter
