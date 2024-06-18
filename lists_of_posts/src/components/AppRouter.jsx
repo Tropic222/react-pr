5bcf3139f8a3f6f52f924b5b7e32fc206296fb79
Tropic222 <deputat1120@gmail.com>
2024-06-18 17:21:56 +0300

    wrote the logic for the authorization page

--- lists_of_posts/src/components/AppRouter.jsx
+++ lists_of_posts/src/components/AppRouter.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { publicRoutes, privateRoutes } from "../router/index";
+import { Navigate } from "react-router-dom";
 
 const AppRouter = () => {
+  const isAuth = false;
+
   return (
+    isAuth
+    ?
       <Routes>
-          {privateRoutes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-          ))}
-          {publicRoutes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-          ))}
+      {privateRoutes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+      ))}
+      {publicRoutes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+      ))}
+            <Route path="/*" element={<Navigate to="/post" replace />} />
+
       </Routes>
+    :
+      <Routes>
+      {publicRoutes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+      ))}
+      <Route path="/*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    
+      
   );
 };
 
