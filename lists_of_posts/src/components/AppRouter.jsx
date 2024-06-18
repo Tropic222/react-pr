c534fe9b0ace465c69bfbd754b7d5d2ba4632f15
Tropic222 <deputat1120@gmail.com>
2024-06-18 17:00:35 +0300

    wrote the authorization page

--- lists_of_posts/src/components/AppRouter.jsx
+++ lists_of_posts/src/components/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { routes } from '../router/index';
-
+import { publicRoutes, privateRoutes } from "../router/index";
 
 const AppRouter = () => {
   return (
       <Routes>
-          {routes.map((route, index) => (
+          {privateRoutes.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+          ))}
+          {publicRoutes.map((route, index) => (
               <Route key={index} path={route.path} element={route.element} />
           ))}
       </Routes>
